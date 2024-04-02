195351d036d13d97f469e8e9887e1dd1898d2728
dparkar00 <67346510+dparkar00@users.noreply.github.com>
2024-04-02 00:47:48 +0000

    added changes

--- src/app.js
+++ src/app.js
@@ -6,6 +6,32 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var suit = ["♥", "♠", "♣", "♦"];
+  var numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  var randomSuit = suit[Math.floor(Math.random() * suit.length)];
+  var randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+
+  document.querySelector(".top-icon").innerHTML = randomSuit;
+  document.querySelector(".card-content").innerHTML = randomNumber;
+  document.querySelector(".bottom-icon").innerHTML = randomSuit;
+
+  if (randomSuit == "♥" || randomSuit == "♦") {
+    document.querySelector(".card").style.color = "red";
+  } else if (randomSuit == "♠" || randomSuit == "♣") {
+    document.querySelector(".card").style.color = "black";
+  }
 };
